f3476fe85722c4e8bc7a74fd08f4084bd57be0b3
sinthuC <sinthu_ch@kkumail.com>
2018-07-20 05:34:26 +0700

    UPDATE page money-detail

--- src/pages/money-detail/money-detail.ts
+++ src/pages/money-detail/money-detail.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the MoneyDetailPage page.
  *
@@ -15,12 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MoneyDetailPage {
   public anArray:any=[];
+  private monthName:string[]
+  private date = new Date();
+  private monthN:string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private storage: Storage) {
+    this.monthName = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+    this.monthN = this.monthName[this.date.getMonth()];
   }
 
+  ngOnInit(){
+  
+  }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MoneyDetailPage');
+
   }
   
   Add(){
