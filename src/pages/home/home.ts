3b685eb080b83c1252d8f6449c0f852de2ca6643
sinthuC <sinthu_ch@kkumail.com>
2018-07-20 05:34:01 +0700

    UPDATE page home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
-
+import { NavController, ModalController,Nav, AlertController } from 'ionic-angular';
+import { Month } from '../../models/month';
+import { Storage } from '@ionic/storage';
+import { MoneyDetailPage } from '../money-detail/money-detail';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  month:Month[];
 
-  constructor(public navCtrl: NavController,public modal:ModalController) {
+  constructor(public navCtrl: NavController,public modal:ModalController,private nav:Nav,private alertCtrl:AlertController,private storage: Storage) {
+   
+    
+  }
+
+  ngOnInit(){
+    this.storage.get('month').then((val)=>{
+      this.month=val;
+      console.log(this.month);
+
+      if(this.month==null){
+        this.month = [];
+      const month_sub = new Month();
+      this.month.push(month_sub);
+      this.presentAlert();
+      }
+
+          });
+
+    
+    
+    
+    
+    
 
   }
+
+ngOnDestroy(){
+ this.saveData();
+}
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'ไม่พบข้อมูลการเงิน',
+      subTitle: 'กรุณาทำการกรอกข้อมูลทางด้านการเงินและจำนวนเงินที่ออม',
+      buttons: [
+        {
+          text:'กรอกข้อมูล',
+          handler : ()=> {
+            this.nav.setRoot(MoneyDetailPage ,{month:this.month}, {animate: true, direction: 'forward'});
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  saveData(){
+    this.storage.set('month', this.month);
+  }
   
+
 }
