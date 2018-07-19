3366db45bdb0f24888fd9f5b484f1e0fe2752bdb
sinthuC <sinthu_ch@kkumail.com>
2018-07-20 05:34:19 +0700

    UPDATE page start

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,Nav } from 'ionic-angular';
 
-import { HomePage } from '../home/home';
+import { HomePage } from '../home/home'
 /**
  * Generated class for the StartPage page.
  *
