c4581e855d64d2b9a5db34ef52a5e1c8a68bb60e
Jormthup <jomtup12@gmail.com>
2018-07-20 10:22:22 +0700

    update modal and home page

--- src/pages/summarymodal/summarymodal.ts
+++ src/pages/summarymodal/summarymodal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the SummarymodalPage page.
@@ -15,11 +15,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SummarymodalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private view: ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SummarymodalPage');
   }
 
+  closeModal(){
+    this.view.dismiss();
+  }
 }
